8fc2efa89b57f0c8ddda989c7b5bc96bc3ee837c
Misha Smirnov <msmirnov@km3net.de>
2021-12-11 03:00:25 +0100

    delete post was finished

--- main.py
+++ main.py
@@ -75,9 +75,11 @@ def get_post(id: int, response: Response):
 
 @app.delete("/posts/{id}", status_code=HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    # logic for deleting post
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)),)
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if deleted_post == None:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f"post with id: {id} doesn't exist")
 
     my_posts.pop(index)
