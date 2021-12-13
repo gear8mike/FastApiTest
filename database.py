22c71916989b3bb2e202d98f149c0e98ad9b7dbc
Misha Smirnov <msmirnov@km3net.de>
2021-12-13 02:47:38 +0100

    time 4:40 database.py is created

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+#example of sql_url
+#SQLALCHEMY_DATABASE_URL = 'postgresql://<username>:<password>@<ip-addresss/hostname>/<database_name>'
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:112121@localhost/fastapi'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
